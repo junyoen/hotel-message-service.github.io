630ab3f52b41514fa4d82c25556ddb6f8d06a614
junyoen <joeykim9010@gmail.com>
2025-02-15 16:23:54 +0900

    Update message.js

--- js/message.js
+++ js/message.js
@@ -88,8 +88,8 @@ async function translateText(text) {
         const response = await fetch(`${API_URL}/translate`, {
             method: 'POST',
             headers: {
-                'Ocp-Apim-Subscription-Key': process.env.TRANSLATOR_KEY,
-                'Ocp-Apim-Subscription-Region': process.env.TRANSLATOR_REGION,
+                'Ocp-Apim-Subscription-Key': TRANSLATOR_KEY,
+                'Ocp-Apim-Subscription-Region': TRANSLATOR_REGION,
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify([{
